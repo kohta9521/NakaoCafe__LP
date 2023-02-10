a025ab036d837595964f6f32ea87f90d8ebec561
kouchikota <kouchikota@kouchikotanoMacBook-Air.local>
2023-02-10 14:58:36 +0900

    add nakaokacafe 2023/02/10

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-/* アドレスバー・ツールバーを除いた100vhの高さを取得 */
 function setHeight() {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty("--vh", `${vh}px`);
@@ -6,7 +5,7 @@ function setHeight() {
   setHeight();
   window.addEventListener("resize", setHeight);
   
-  /* スライダーの設定 */
+
   const verticalSlider = new Swiper(".vertical-slider", {
     direction: "vertical",
     slidesPerView: 1,
@@ -18,3 +17,25 @@ function setHeight() {
       clickable: true,
     },
   });
+
+  let data = [
+    { name: '参加人数', value: 30 },
+  ];
+  
+  // しきい値
+  let threshold = 100;
+  
+  for (var variable of data) {
+  
+    // しきい値からパーセンテージを計算
+    variable.percentage = Math.round(variable.value / threshold * 100);
+  
+    // containerに追加
+    document.querySelector('#container').insertAdjacentHTML('beforeend', `<div class="graf">
+    <div><span class="data-name">${variable.name}</span></div>
+    <div class="graf-bar-bg">
+    <div class="graf-bar" id="${variable.name}"><span>${variable.value}</span></div>
+    </div></div>`);
+    // widthを指定
+    document.querySelector('#' + variable.name).style.width = variable.percentage + '%';
+  }
